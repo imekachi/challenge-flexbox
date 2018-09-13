e6eed30633947dc8fcb65e2ca0182a28d393fb91
imekachi <saran@dek-d.com>
2018-09-13 16:02:35 +0700

    add hot reload functionality

--- src/index.js
+++ src/index.js
@@ -4,5 +4,14 @@ import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />, rootElement)
 registerServiceWorker()
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    // eslint-disable-next-line global-require
+    const UpdatedApp = require('./App').default
+    ReactDOM.render(<UpdatedApp />, rootElement)
+  })
+}
